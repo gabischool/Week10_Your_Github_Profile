a7c47294b97e563ce03ae6512c73541b4bfc4ec6
Abdiwali Mohamed <duranabdi@gmail.com>
2023-05-05 20:37:55 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom"
+
+// import "BrowserRouter" from "react-router-dom"
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* Wrap the "App" component with "BrowserRouter" */}
+    {/* "BrowserRouter" hugi "App" */}
+    
     <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
