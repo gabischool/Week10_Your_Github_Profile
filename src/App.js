3ff65f8b6208d9ecac20959a0ef44882de3c3481
Ali Omar Abdi <alikey2023@gmail.com>
2023-05-16 19:28:11 +0300

    My Frist Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
 
 // Import the "useState" and "useEffect" hooks from React
 // Soo jiido "useState" iyo "useEffect"
+import React, { useState , useEffect } from "react";
 
 // Import the "MyProfile" component
 // Soo jiido "MyProfile" component-ka
+import MyProfile from './components/MyProfile'
 
 // Import the "axios" library
 // Soo jiido "axios" hoos
+import axios from 'axios';
 
 import "./App.css";
 
@@ -15,6 +17,9 @@ function App() {
 
   // Create 3 states, "profile", "followers", and "following"
   // Samee 3 state, "profile", "followers", iyo "following"
+  const [profile, setProfile] = useState([]);
+  const [followers, setFollowers] = useState([]);
+  const [following, setFollowing] = useState([]);
 
 
   // API For Profile = https://api.github.com/users/<your-github-username>
@@ -23,14 +28,44 @@ function App() {
 
   // Use axios to fetch data from the API using the useEffect hook
   // Halkaan isticmaal axios adigoo kasoo jiidanaayo waxaa u baahantahay API, useEffect hook-na isticmaal
+  useEffect(() => {
+    axios.get('https://api.github.com/users/Ali-Key')
+     .then((res) => {
+      //  console.log(res.data);
+       setProfile(res.data);
+     })
+     .catch((err) => {
+       console.log(err);
+     })
+
+    axios.get('https://api.github.com/users/Ali-Key/followers')
+     .then((res) => {
+      // console.log(res);
+      setFollowers(res.data)
+     })
+     .catch((err) => {
+      console.log(err);
+     })
+
+    axios.get('https://api.github.com/users/Ali-Key/following')
+      .then((response) => {
+        // console.log(response.data);
+        setFollowing(response.data)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
+
+    
+  }, [])
     
   return (
     <div className="bg-white md:mx-auto rounded shadow-xl w-full md:w-1/2 overflow-hidden">
-      <div className="h-[140px] bg-gradient-to-r from-cyan-500 to-blue-500"></div>
-
+      <div className="h-[140px] bg-gradient-to-r from-cyan-500 to-blue-700"></div>
       {/* Show "MyProfile" component here and give it 3 props, "profile", "followers", and "following" */}
       {/* Halkaan soo gali "MyProfile", 3 props-na sii, "profile", "followers", iyo "following" */}
-
+      <MyProfile profile={profile} followers={followers} following={following} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom/client';
 
 // import "BrowserRouter" from "react-router-dom"
 
+import { BrowserRouter } from'react-router-dom';
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
     {/* Wrap the "App" component with "BrowserRouter" */}
     {/* "BrowserRouter" hugi "App" */}
     
-    <App />
+
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+    
+    
   </React.StrictMode>
 );
