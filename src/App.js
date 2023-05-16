b17cee73584fbaef9d2529c7003406e24bbf2512
Miirshe <miirshe@gmail.com>
2023-05-16 22:38:08 +0300

    Im make a Big change for this assigmnet & also completed all of

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-
+import React, { useState , useEffect } from "react";
 // Import the "useState" and "useEffect" hooks from React
 // Soo jiido "useState" iyo "useEffect"
 
 // Import the "MyProfile" component
 // Soo jiido "MyProfile" component-ka
 
+import MyProfile from './components/MyProfile'
+
 // Import the "axios" library
 // Soo jiido "axios" hoos
+import axios from 'axios';
 
 import "./App.css";
 
@@ -15,6 +17,10 @@ function App() {
 
   // Create 3 states, "profile", "followers", and "following"
   // Samee 3 state, "profile", "followers", iyo "following"
+  const [profile , setprofile] = useState([]);
+  const [followers , setfollowers] = useState([]);
+  const [following , setfollowing] = useState([]);
+  const [overview, setoverview] = useState([]);
 
 
   // API For Profile = https://api.github.com/users/<your-github-username>
@@ -23,6 +29,16 @@ function App() {
 
   // Use axios to fetch data from the API using the useEffect hook
   // Halkaan isticmaal axios adigoo kasoo jiidanaayo waxaa u baahantahay API, useEffect hook-na isticmaal
+  useEffect(()=>{
+    axios.get('https://api.github.com/users/miirshe')
+    .then(res=>{ setprofile(res.data) }).catch(err=>{ console.log(err)});
+    axios.get('https://api.github.com/users/miirshe/followers').then(res=>{ setfollowers(res.data)})
+    .catch(err=>{ console.log(err)});
+    axios.get('https://api.github.com/users/miirshe/following').then(res=>{ setfollowing(res.data)})
+    .catch(err=>{ console.log(err)});
+    axios.get('https://api.github.com/users/miirshe/repositories').then(res=>{ setoverview(res.data)})
+    .catch(err=>{ console.log(err)});
+  },[])
     
   return (
     <div className="bg-white md:mx-auto rounded shadow-xl w-full md:w-1/2 overflow-hidden">
@@ -30,6 +46,7 @@ function App() {
 
       {/* Show "MyProfile" component here and give it 3 props, "profile", "followers", and "following" */}
       {/* Halkaan soo gali "MyProfile", 3 props-na sii, "profile", "followers", iyo "following" */}
+      <MyProfile profile={profile} followers={followers} following={following} overview={ overview } />
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* Wrap the "App" component with "BrowserRouter" */}
     {/* "BrowserRouter" hugi "App" */}
-    
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
   </React.StrictMode>
 );
