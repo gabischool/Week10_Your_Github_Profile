4edf9e9305b01cdb5ccdd6562ca8b204bbb07697
Xabiib Ali <119079937+xabiibali@users.noreply.github.com>
2023-05-19 15:40:33 +0300

    waan dhameeyay

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // import "BrowserRouter" from "react-router-dom"
-
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -11,7 +11,8 @@ root.render(
   <React.StrictMode>
     {/* Wrap the "App" component with "BrowserRouter" */}
     {/* "BrowserRouter" hugi "App" */}
-    
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
