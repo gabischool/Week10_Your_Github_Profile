53333e60288673f0edefb27d4a2a628a37c90f6b
Hani Nuur Bocor <116567403+Hanaafilsan@users.noreply.github.com>
2023-06-02 22:18:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// import "BrowserRouter" from "react-router-dom"
+
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* Wrap the "App" component with "BrowserRouter" */}
+    {/* "BrowserRouter" hugi "App" */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+  </React.StrictMode>
+);
